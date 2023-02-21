26f458721f004b69d6db77bfffc37b63fd2236d3
abhinav123123 <72694681+abhinav123123@users.noreply.github.com>
2023-02-22 00:22:07 +0530

    Update express.js

--- express.js
+++ express.js
@@ -2,15 +2,18 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const template=require('./path/path');
-const bodyParser=require("body-parser");
+const bodyParser=require('body-parser');
 const {adminRouter}=require("./router/admin");
 const { shopRouter }=require("./router/shop");
-app.use(bodyParser.urlencoded({extended:false}));
+const {error404}=require('./controller/error');
 
+// set templating engine
+app.set('view engine','ejs');
+app.set('views','views');
+app.use(express.urlencoded({extended:true}));//also express.urlencoded work
+app.use(express.static(path.join(template,'public')));
 app.use('/admin',adminRouter);
 app.use(shopRouter); 
 
-app.use((req,res)=>{
-  res.status(404).sendFile(path.join(template,'views','404.html'));
-})
+app.use(error404);
 app.listen(5000);
